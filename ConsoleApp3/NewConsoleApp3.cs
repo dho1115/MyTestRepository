331a68522b303a06d27982d559926a25a8dd46a1
David H <davidhorph@hotmail.com>
2018-07-01 10:20:18 -0500

    recent 7/1 changes

---
+++ ConsoleApp3/NewConsoleApp3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is test 2!!! test 2!!! Another change.");
+            Console.WriteLine("AND ANOTHER ONE AS OF 7/1!!!!!");
+            Console.WriteLine("More!!!");
+        }
+    }
+}
